7cb3daec59d7815fb502b039dc027430a103e1d6
stevenshelley58-afk <stevenshelley58@gmail.com>
2025-10-08 13:00:21 +0800

    Deploy to GitHub Pages

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 export const config = {
-  supabaseUrl: "https://qygcwfhtzgrhllegnxaw.supabase.co/functions/v1/ingest-master-data",
+  supabaseUrl: "https://qygcwfhtzgrhllegnxaw.supabase.co",
   supabaseAnonKey: "2735551a978a78882e4c40249a20fcce39ad160f6b73c3cef1fe56f8913618b0",
 };
